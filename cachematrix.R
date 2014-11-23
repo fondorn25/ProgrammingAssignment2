b5a8daf36478dd62279d24a53d28f4b93d9e6004
fondorn25 <chech_mot@yahoo.com>
2014-11-23 18:00:41 -0500

    Added comments
    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -4,13 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        ##value of S gets set to NULL when new matrix is created
   s <- NULL
+        
   set <- function(y) {
     x <<- y
     s <<- NULL
   }
+        ##get function returns value of original matrix
   get <- function() x
+        ##sets S value when called by cacheSolve
   setsolve <- function(solve) s <<- solve
+        ##gets cached S value when called by cacheSolve
   getsolve <- function() s
   list(set = set, get = get,
        setsolve = setsolve,
@@ -23,12 +28,15 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
   s <- x$getsolve()
+        ##check for value of S - if S is not NULL - cached data is pulled
   if(!is.null(s)) {
     message("getting cached data")
     return(s)
   }
+        ##if S is NULL - S is being set to inverse of the matrix
   data <- x$get()
   s <- solve(data, ...)
+        ##Calculated S value is being cached with setsolve
   x$setsolve(s)
   s
 }
